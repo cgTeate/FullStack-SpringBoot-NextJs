f03da2380a6a38bf6e455a7dc0f15a3653cab059
Calvin Teater <laughingglint3@gmail.com>
2022-09-08 18:32:35 -0500

    springboot setup

---
+++ src/main/java/git/cgteate/fullstackspringbootnext/FullstackspringbootnextApplication.java
@@ -0,0 +1,13 @@
+package git.cgteate.fullstackspringbootnext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackspringbootnextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackspringbootnextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/cgteate/fullstackspringbootnext/FullstackspringbootnextApplicationTests.java
@@ -0,0 +1,13 @@
+package git.cgteate.fullstackspringbootnext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackspringbootnextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
