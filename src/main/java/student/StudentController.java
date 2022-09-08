e6b1a162869f535464e417ead0761b8b272d72d4
Calvin Teater <laughingglint3@gmail.com>
2022-09-08 18:56:32 -0500

    created StudentController

---
+++ src/main/java/student/StudentController.java
@@ -0,0 +1,31 @@
+package student;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+    
+    @GetMapping
+    public List<Student> getAllStudents(){
+        List<Student> students = Arrays.asList(
+            new Student(
+                1L, 
+                "Calvin", 
+                "calvin.teater@washburn.edu",
+                Gender.MALE),
+            new Student(
+                2L, 
+                "Je'Shon", 
+                "jeshon.edwards@washburn.edu",
+                Gender.MALE)
+        );
+        return students;
+    }
+
+}
